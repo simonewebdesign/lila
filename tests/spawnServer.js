e0ceb183f28384b44e12f434cb901c207c113189
Simone Vittori <hello@simonewebdesign.it>
2020-04-16 19:43:22 +0100

    Add spawnServers

--- tests/spawnServer.js
+++ tests/spawnServer.js
@@ -1,12 +1,4 @@
-const numberOfServers = parseInt(process.argv[2]) || 7
-
-const serverNames = Array.from({ length: numberOfServers }, (_, index) =>
-  String.fromCharCode('a'.charCodeAt(0) + index)
-)
-
-const servers = serverNames.map((name, index) =>
-  spawnServer(name, 8000 + index)
-)
+const http = require('http')
 
 function spawnServer(name, port) {
   return http
